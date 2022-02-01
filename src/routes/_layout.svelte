99157546288b20edcad4a1cee4bfb382abe25ef8
Vincent Boulaye <vincent.boulaye@orange.com>
2022-02-01 08:47:25 +0100

    init

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+	import {QueryClient, QueryClientProvider} from "@sveltestack/svelte-query";
+
+	export let segment;
+
+	const queryClient = new QueryClient({ })
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<QueryClientProvider client={queryClient}>
+		<slot></slot>
+	</QueryClientProvider>
+</main>
